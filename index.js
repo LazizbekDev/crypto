b40c4f63f35ac48c1727557b39302645f2100705
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-03-03 19:19:15 +0500

    daxshat, telegram bot o'ta zo'r

--- index.js
+++ index.js
@@ -9,4 +9,26 @@ bot.start((ctx) => {
     ctx.reply('SALOM')
 })
 
+bot.command('test', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Yaxshi, unda tanla", {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: "Obunachilar", callback_data: "mavjud_emas"},
+                    {text: "Admin", callback_data: "admin"}
+                ],
+            ]
+        }
+    })
+})
+
+bot.action('mavjud_emas', ctx => {
+    ctx.answerCbQuery('Bot test rejimida ishlamoqda');
+    ctx.reply('Hali abunachilar mavjud emas, bot test rejimida ishlamoqda!')
+})
+bot.action('admin', ctx => {
+    ctx.answerCbQuery('Bot test rejimida ishlamoqda');
+    ctx.reply('Hali Amdin mavjud emas, bot test rejimida ishlamoqda!')
+})
+
 bot.launch();
