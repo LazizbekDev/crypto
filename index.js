3ae77b27ffdb7cc868451b09eaddefcbf6643b30
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-03-03 20:18:11 +0500

    coin buttons

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ bot.start(ctx => {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: "Crypto Prices", callback_data: "price "},
+                    { text: "Crypto Prices", callback_data: "price"},
                     { text: "CoinMarketCap", url: "https://www.cryptocompare.com"}
                 ],
                 [
                     { text: "About me", url: "https://www.lazizbe.uz"}
+                ]
+            ]
+        }
+    })
+})
+
+bot.action("price", (ctx) => {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Get price information', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: "BTC", callback_data: "price-BTC"},
+                    {text: "ETC", callback_data: "price-ETC"}
+                ],
+                [
+                    {text: "BCH", callback_data: "price-BCH"},
+                    {text: "LTC", callback_data: "price-LTC"}
+                ],
+                [
+                    {text: "Back to main page", callback_data: "start"}
                 ]
             ]
         }
