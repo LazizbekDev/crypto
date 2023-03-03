edf6968824b4d6d0f1a94b4cbd55203d94ab0c51
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-03-03 20:03:31 +0500

    it's going so awesome

--- index.js
+++ index.js
@@ -4,31 +4,22 @@ import { config } from "dotenv";
 config();
 
 const bot = new Telegraf(process.env.TOKEN);
+const API = process.env.API;
 
-bot.start((ctx) => {
-    ctx.reply('SALOM')
-})
-
-bot.command('test', ctx => {
-    bot.telegram.sendMessage(ctx.chat.id, "Yaxshi, unda tanla", {
+bot.start(ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, 'Heyy welcome to crypto bot', {
         reply_markup: {
             inline_keyboard: [
                 [
-                    {text: "Obunachilar", callback_data: "mavjud_emas"},
-                    {text: "Admin", callback_data: "admin"}
+                    { text: "Crypto Prices", callback_data: "price "},
+                    { text: "CoinMarketCap", url: "https://www.cryptocompare.com"}
                 ],
+                [
+                    { text: "About me", url: "https://www.lazizbe.uz"}
+                ]
             ]
         }
     })
-})
-
-bot.action('mavjud_emas', ctx => {
-    ctx.answerCbQuery('Bot test rejimida ishlamoqda');
-    ctx.reply('Hali abunachilar mavjud emas, bot test rejimida ishlamoqda!')
-})
-bot.action('admin', ctx => {
-    ctx.answerCbQuery('Bot test rejimida ishlamoqda');
-    ctx.reply('Hali Amdin mavjud emas, bot test rejimida ishlamoqda!')
 })
 
 bot.launch();
